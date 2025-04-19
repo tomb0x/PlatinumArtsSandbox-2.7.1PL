bce4d4d13f94b2e4721b8b20414f6950982d4f9c
rodzice <wptom@wp.pl>
2025-04-19 14:52:58 +0200

    zmiana w sandbox.sh

--- sandbox.sh
+++ sandbox.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# ---------------------------------------------------------------
+# główny plik uruchamiający dla systemów Linux
+# ---------------------------------------------------------------
+# tutaj można wybrać, który tryb gry jest domyślnie uruchamiany
+# poleceniem sandbox z tego katalogu (./sandbox)
+# ---------------------------------------------------------------
 ./sandbox_fps.sh
